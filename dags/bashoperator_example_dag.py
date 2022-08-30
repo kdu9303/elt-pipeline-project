38658a3afa084b6ab99cfceea0ed84229af49559
kdu9303 <zheros9303@gmail.com>
2022-08-30 19:56:01 +0900

    add test dag

---
+++ dags/bashoperator_example_dag.py
@@ -0,0 +1,34 @@
+import pendulum
+from airflow.decorators import dag
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'email': ['zheros9303@gmail.com'],
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    tags=['test dag']
+)
+def print_python_version():
+    """
+    ### Dag Documentation
+    python version을 프린트하는 test dag 입니다.
+    """
+    execute_cmd = BashOperator(
+        task_id='shell_execute',
+        bash_command="echo python --version"
+    )
+
+    execute_cmd
+
+
+dag = print_python_version()
