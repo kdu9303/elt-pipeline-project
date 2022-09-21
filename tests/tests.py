b2598dc58bcc7302e3fcabc0d2986cfb9970b60f
kdu9303 <zheros9303@gmail.com>
2022-09-21 10:33:07 +0900

    delete sub-directory path

--- tests/tests.py
+++ tests/tests.py
@@ -7,9 +7,6 @@ from airflow.models import DagBag
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../dags"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../dags/modules"))
-sys.path.append(
-    os.path.join(os.path.dirname(__file__), "../dags/modules/newsscraper")
-)
 
 # Airflow variables called from DAGs under test are stubbed out
 # os.environ["AIRFLOW_DATA_LAKE_BUCKET"] = ""
