157aca5d94d44c848033977f8bb778fe9e1e9e64
kdu9303 <zheros9303@gmail.com>
2022-10-06 21:11:33 +0900

    spark job test dag updated

--- dags/test_spark_job_dag.py
+++ dags/test_spark_job_dag.py
@@ -26,17 +26,21 @@ default_args = {
     tags=["spark job test dag"],
     catchup=False,
 )
-def spark_job_http_request_test(file_name):
+def spark_job_http_request_test():
     """
     ### Dag Documentation
     Livy rest api를 통해 spark job을 할당해주는 dag입니다.
     """
+    # spark job script
+    file_name = "spark_job_test.py"
+
     remote_file_path = f"/home/ec2-user/spark-data/{file_name}"
 
+    # Airflow Server에서 Spark driver Server로 스크립트 전송
     transter_python_script = SFTPOperator(
         task_id="transter_python_script",
         ssh_conn_id="spark_master_host_connection",
-        local_filepath="/opt/airflow/dags/modules/{file_name}",
+        local_filepath=f"/opt/airflow/dags/modules/{file_name}",
         remote_filepath=remote_file_path,
         operation="put",
         create_intermediate_dirs=True,
@@ -46,7 +50,7 @@ def spark_job_http_request_test(file_name):
     def run_spark_batch_job():
         postfix = datetime.now().strftime("%Y%m%d-%H%M%S")
         # 배치 이름이 겹치면 오류
-        batch_name = f"run-{file_name}-spark_job-{postfix}"
+        batch_name = f"<{file_name}>-spark_job-{postfix}"
         spark_host_local_file_path = f"file:{remote_file_path}"
 
         livy_hook = LivyHook(livy_conn_id="livy_connection")
@@ -66,7 +70,7 @@ def spark_job_http_request_test(file_name):
             state = livy_hook.get_batch_state(batch_id)
 
         logger.info(
-            "Batch with id {batch_id} terminated with state: {state.value}"
+            f"Batch with id {batch_id} terminated with state: {state.value}"
         )
 
         if state != BatchState.SUCCESS:
@@ -78,4 +82,4 @@ def spark_job_http_request_test(file_name):
     transter_python_script >> run_spark_batch_job()
 
 
-dag = spark_job_http_request_test("spark_job_test.py")
+dag = spark_job_http_request_test()
