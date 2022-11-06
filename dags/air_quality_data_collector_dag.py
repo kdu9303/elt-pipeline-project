4be631b0b42cd3964b2297c15996941df7d399a6
kdu9303 <zheros9303@gmail.com>
2022-11-06 23:53:17 +0900

    air quality dag add tasks

--- dags/air_quality_data_collector_dag.py
+++ dags/air_quality_data_collector_dag.py
@@ -6,6 +6,9 @@ from airflow.decorators import dag, task
 from airflow.exceptions import AirflowException
 from modules.producer import MessageProducer
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.providers.sftp.operators.sftp import SFTPOperator
+from airflow.providers.sftp.sensors.sftp import SFTPSensor
+from modules.spark_job_livy_custom_operator import SparkSubmitOperator
 from modules.air_quality_statistics.module_air_quality_collector import (
     AirQualityDataScraper,
 )
@@ -36,8 +39,8 @@ def scrape_air_quality_data():
 
     Task 순서:
         1. OPEN API에서 지역별 자료를 호출하여 Kafka producer로 넘긴다.
-
         2. S3 bucket에 해당일자 partition에 자료가 도착했는지 확인한다.
+
         3. Airflow Server에서 Spark driver server로 py file 스크립트를 전송한다.
         4. Spark remote server에 py파일이 도착했는지 확인한다.
         5. Livy rest api를 통해 spark job을 할당한다.
@@ -82,8 +85,46 @@ def scrape_air_quality_data():
         poke_interval=60,
     )
 
+    # SSH File Transfer
+    file_name = "spark_job_air_quality_data_collector.py"
+    local_file_path = (
+        f"/opt/airflow/dags/modules/air_quality_statistics/{file_name}"
+    )
+    remote_file_path = f"/home/ec2-user/spark-data/{file_name}"
+
+    transter_python_script = SFTPOperator(
+        task_id="transter_python_script",
+        ssh_conn_id="spark_master_host_connection",
+        local_filepath=local_file_path,
+        remote_filepath=remote_file_path,
+        operation="put",
+        create_intermediate_dirs=True,
+    )
+
+    # Remote File Sensor
+    spark_script_file_checker = SFTPSensor(
+        task_id="spark_script_file_checker",
+        sftp_conn_id="spark_master_host_connection",
+        path=remote_file_path,
+        poke_interval=10,
+    )
+
+    run_spark_batch_job = SparkSubmitOperator(
+        task_id="run_spark_batch_job",
+        livy_conn_id="livy_connection",
+        file_name=file_name,
+        polling_interval=30,
+        delete_session=False,
+    )
+
     # task flow
-    (produce_data_to_broker() >> S3_data_upload_checker)  # noqa: W503
+    (
+        produce_data_to_broker()
+        >> S3_data_upload_checker  # noqa: W503
+        >> transter_python_script  # noqa: W503
+        >> spark_script_file_checker  # noqa: W503
+        >> run_spark_batch_job  # noqa: W503
+    )
 
 
 dag = scrape_air_quality_data()
