e27a868c9a178e614c44f7a0bb12d8df78e96094
kdu9303 <zheros9303@gmail.com>
2022-11-10 20:56:38 +0900

    config file upload

--- dags/modules/spark_job_test.py
+++ dags/modules/spark_job_test.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from pyspark.sql import SparkSession
 import os
 import configparser
+from typing import Dict
+from pyspark.sql import SparkSession
 
 
-def getAWSKeys(profile):
+def get_aws_keys(profile: str) -> Dict[str, str]:
     awsCreds = {}
     Config = configparser.ConfigParser()
     Config.read(os.path.expanduser("/home/ec2-user/.aws/credentials"))
@@ -18,21 +19,52 @@ def getAWSKeys(profile):
     return awsCreds
 
 
-awsKeys = getAWSKeys("default")
+awsKeys = get_aws_keys("default")
 Access_key_ID = awsKeys["aws_access_key_id"]
 Secret_access_key = awsKeys["aws_secret_access_key"]
-
+# -------------------------------------------------------------------
+# spark config
+# -------------------------------------------------------------------
 spark = (
     SparkSession.builder.master("yarn")
-    .appName("test")
-    .config("spark.driver.memory", "4g")
+    .config("spark.submit.deployMode", "cluster")
+    .appName("Census_Data_Collection_Transfrom")  # spark history에서 구분자 사용
+    .config("spark.driver.memory", "2g")
+    .config("spark.driver.cores", 1)
+    .config(
+        "spark.driver.extraJavaOptions",
+        "-Dlog4j.configuration=log4j.properties",
+    )
+    .config("spark.executor.instances", 2)
     .config("spark.executor.memory", "2g")
-    .config("spark.yarn.am.memory", "2g")
-    .config("spark.executor.cores", 3)
-    .config("spark.executor.instances", 1)
+    .config(
+        "spark.executor.extraJavaOptions",
+        "-Dlog4j.configuration=log4j.properties",
+    )
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.0.0")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+    .config("spark.databricks.delta.schema.autoMerge.enabled", "true")
     .getOrCreate()
 )
 
+# delta lake package path
+spark.sparkContext.addPyFile(
+    "/home/ec2-user/spark/jars/delta-core_2.12-2.0.0.jar"
+)
+
+# log4j property file
+spark.sparkContext.addFile("/home/ec2-user/spark/conf/log4j.properties")
+
+# spark session을 생성한 이후 delta lake 모듈을 불러올 수 있음
+from delta.tables import *  # noqa: E402, F401, F403, F405
+
+# -------------------------------------------------------------------
+# hadoop s3a config
+# -------------------------------------------------------------------
 hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
 hadoop_conf.set("com.amazonaws.services.s3.enableV4", "true")
 hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
@@ -47,6 +79,11 @@ hadoop_conf.set("fs.s3a.access.key", Access_key_ID)
 hadoop_conf.set("fs.s3a.secret.key", Secret_access_key)
 hadoop_conf.set("fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com")
 hadoop_conf.set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.4")
+hadoop_conf.set(
+    "spark.delta.logStore.class",
+    "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
+)
+
 
 S3_ACCESS_POINT_ALIAS = (
     "etl-project-bucket-os3osysqodthpasbb5yqre5ot7jeaapn2a-s3alias"
@@ -66,3 +103,6 @@ df = (
 
 df.show()
 df.printSchema()
+
+
+spark.stop()
